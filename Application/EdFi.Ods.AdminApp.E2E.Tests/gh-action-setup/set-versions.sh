e550dc5cd5a0c30c7559181fe8a3878c7bd07897
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2024-03-21 11:54:24 -0600

    Trying to fix e2e tests 7.

--- Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
+++ Application/EdFi.Ods.AdminApp.E2E.Tests/gh-action-setup/set-versions.sh
@@ -15,6 +15,7 @@ echo "Using $DB Version: $DB_VERSION"
 sed -i -E "s/ENV ADMINAPP_DATABASE_VERSION=\"[0-9]+.[0-9]+.[0-9]+(-alpha\.+[0-9]+.[0-9]+)?\"/ENV ADMINAPP_DATABASE_VERSION=\"$DB_VERSION\"/w changelog.txt" ods-docker/DB-Admin/Alpine/pgsql/Dockerfile
 
 # sed -i -E "s/ARG VERSION=\"[0-9]+.[0-9]+.[0-9]+(-alpha\.+[0-9]+.[0-9]+)?\"/ARG VERSION=\"$WEB_VERSION\"/w changelog.txt" main/Docker/pgsql.Dockerfile
+sed -i -E "s/ARG VERSION=ARG VERSION=latest/ARG VERSION=\"$WEB_VERSION\"/w changelog.txt" main/Docker/pgsql.Dockerfile
 
 if [ -s changelog.txt ]; then
     echo "Files updated"
